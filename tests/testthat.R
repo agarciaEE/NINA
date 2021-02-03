acf501bd85b9bdfcd12cd586f4219631fe6e05ea
agarciaEE <agarcia26286@gmail.com>
2021-02-03 20:01:23 +0100

    Added some tests

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(NINA)
+
+test_check("NINA")
---
+++ tests/testthat/test_create_projectionR.R
@@ -0,0 +1,14 @@
+context("Testing")
+
+test_that("Succes", {
+
+  set.seed(1)
+  res = raster_projection(data.frame(x = 1:100, y = 1:100, z = rnorm(100), w =  rbinom(100, 1,.5)))
+
+  expect_equal(class(res)[1], "RasterStack")
+
+  cm = raster::as.matrix(res[[1]])
+  expect_equal(dim(cm), c(100, 100))
+
+  expect_equal(raster::nlayers(res), 2)
+})
