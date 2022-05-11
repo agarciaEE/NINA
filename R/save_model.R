aa0e61e3038bd3369ec5a515c7a03cc3975fff31
agarciaEE <agarcia26286@gmail.com>
2022-05-11 15:11:20 +0200

    minor modifications

--- R/save_model.R
+++ R/save_model.R
@@ -18,120 +18,128 @@
 #' @export
 save_model <- function(m, path = "~", project.name = "NINA1"){
 
-  if (class(m)[1] != "NINA" && !class(m)[2] %in% c("ENmodel", "BCmodel", "ECmodel")){
-    stop("Object if not a NINA model")
-  }
-  paths = list()
-  ### initiate
-  path = file.path(path, project.name)
-  dir.create(path)
-  #paths <- list(z = file.path(path, "z"),
-  #              zglob = file.path(path, "zglobal"),
-  #              sd = file.path(path, "sd"),
-  #              info= file.path(path, "info"),
-  #              data = file.path(path, "data"),
-  #              eval = file.path(path, "eval"))
-  #sapply(paths, dir.create)
-  write.table(class(m), file = paste0(path, "/class.txt"), row.names = F, col.names = F)
-  #### zmod
-  zmod = m$z.mod
-  npath = file.path(path, "z")
-  dir.create(npath)
-  if (is.list(zmod)){
-    for (r in names(zmod)){
-      if (all(class(zmod[[r]]) == c("NINA", "niche"))){
-        write_niche(zmod[[r]], path = npath, file = gsub("\\/", " ", r))
-      }
-      else if (is.list(zmod[[r]])){
-        dir.create(path = file.path(npath, sub("\\/", " ", r)))
-        for (s in names(zmod[[r]])){
-          if (all(class(zmod[[r]][[s]]) == c("NINA", "niche"))){
-            write_niche(zmod[[r]][[s]], path = file.path(npath,sub("\\/", " ", r)), file = s)
-          }
-        }
-      }
+  if (class(m)[1] == "NINA" && class(m)[2] == "modelsList") {
+    path = file.path(path, project.name)
+    dir.create(path)
+    for (l in names(m)) {
+      save_model(m[[l]], path = path, project.name = l)
     }
   }
-  ####
-  #### zmod global
-  if (!is.null(m$z.mod.global)){
-    zglob = m$z.mod.global
-    npath = file.path(path, "zglobal")
+  else if (class(m)[1] == "NINA" && class(m)[2] %in% c("ENmodel", "BCmodel", "ECmodel")){
+    paths = list()
+    ### initiate
+    path = file.path(path, project.name)
+    dir.create(path)
+    #paths <- list(z = file.path(path, "z"),
+    #              zglob = file.path(path, "zglobal"),
+    #              sd = file.path(path, "sd"),
+    #              info= file.path(path, "info"),
+    #              data = file.path(path, "data"),
+    #              eval = file.path(path, "eval"))
+    #sapply(paths, dir.create)
+    write.table(class(m), file = paste0(path, "/class.txt"), row.names = F, col.names = F)
+    #### zmod
+    zmod = m$z.mod
+    npath = file.path(path, "z")
     dir.create(npath)
-    if (is.list(zglob)){
-      for (r in names(zglob)){
-        if (all(class(zglob[[r]]) == c("NINA", "niche"))){
-          write_niche(zglob[[r]], path = npath, file = gsub("\\/", " ", r))
+    if (is.list(zmod)){
+      for (r in names(zmod)){
+        if (all(class(zmod[[r]]) == c("NINA", "niche"))){
+          write_niche(zmod[[r]], path = npath, file = gsub("\\/", " ", r))
         }
-        else if (is.list(zglob[[r]])){
+        else if (is.list(zmod[[r]])){
           dir.create(path = file.path(npath, sub("\\/", " ", r)))
-          for (s in names(zglob[[r]])){
-            if (all(class(zglob[[r]][[s]]) == c("NINA", "niche"))){
-              write_niche(zglob[[r]][[s]], path = file.path(npath,sub("\\/", " ", r)), file = s)
+          for (s in names(zmod[[r]])){
+            if (all(class(zmod[[r]][[s]]) == c("NINA", "niche"))){
+              write_niche(zmod[[r]][[s]], path = file.path(npath,sub("\\/", " ", r)), file = s)
             }
           }
         }
       }
     }
-  }
-  ####
-  #### w
-  if(!is.null(m$w)){
-    wmod = m$w
-    npath = file.path(path, "w")
-    dir.create(npath)
-    if (is.list(m$w)){
-      for (r in names(m$w)){
-        if (all(class(m$w[[r]]) == c("NINA", "niche"))){
-          write_niche(m$w[[r]], path = npath, file = gsub("\\/", " ", r))
-        }
-        else if (is.list(m$w[[r]])){
-          dir.create(path = file.path(npath, sub("\\/", " ", r)))
-          for (s in names(m$w[[r]])){
-            if (all(class(m$w[[r]][[s]]) == c("NINA", "niche"))){
-              write_niche(m$w[[r]][[s]], path = file.path(npath,sub("\\/", " ", r)), file = s)
+    ####
+    #### zmod global
+    if (!is.null(m$z.mod.global)){
+      zglob = m$z.mod.global
+      npath = file.path(path, "zglobal")
+      dir.create(npath)
+      if (is.list(zglob)){
+        for (r in names(zglob)){
+          if (all(class(zglob[[r]]) == c("NINA", "niche"))){
+            write_niche(zglob[[r]], path = npath, file = gsub("\\/", " ", r))
+          }
+          else if (is.list(zglob[[r]])){
+            dir.create(path = file.path(npath, sub("\\/", " ", r)))
+            for (s in names(zglob[[r]])){
+              if (all(class(zglob[[r]][[s]]) == c("NINA", "niche"))){
+                write_niche(zglob[[r]][[s]], path = file.path(npath,sub("\\/", " ", r)), file = s)
+              }
             }
           }
         }
       }
     }
-  }
-  ####
-  #### species distribution
-  sd <- m$pred.dis
-  npath = file.path(path, "sd")
-  dir.create(npath)
-  write.table(sd, file = paste0(npath, "/species_distributions.txt"))
-  sapply(names(m$maps) , function(s) writeRaster(m$maps[[s]], filename = paste0(npath, "/", gsub("\\.", "_", s)), format = "GTiff", overwrite = T))
-  ####
-  #### info
-
-  npath = file.path(path, "info")
-  dir.create(npath)
-  write.table(m$tab, file = paste0(npath, "/tab.txt"))
-  write.table(m$fail, file = paste0(npath, "/fail.txt"))
-  write.table(m$predictors, file = paste0(npath, "/predictors.txt"), row.names = F, col.names = F)
-  write.table(as.character(m$crs), file = paste0(npath, "/crs.txt"))
-  ####
-  #### data
-
-  npath = file.path(path, "data")
-  dir.create(npath)
-  if (!is.null(m$clus)){
-    write.table(m$clus, file = paste0(npath, "/regions.txt"))
-  }
-  write.table(m$obs, file = paste0(npath, "/occurrences.txt"))
-  write.table(m$sp.scores, file = paste0(npath, "/sp_scores.txt"))
-  write.table(m$env.scores, file = paste0(npath, "/env_scores.txt"))
-  saveRDS(m$pca, file = paste0(npath, "/pca.RDS"))
-
-  ####
-  #### eval
-  if (!is.null(m$eval)){
-    npath = file.path(path, "eval")
+    ####
+    #### w
+    if(!is.null(m$w)){
+      wmod = m$w
+      npath = file.path(path, "w")
+      dir.create(npath)
+      if (is.list(m$w)){
+        for (r in names(m$w)){
+          if (all(class(m$w[[r]]) == c("NINA", "niche"))){
+            write_niche(m$w[[r]], path = npath, file = gsub("\\/", " ", r))
+          }
+          else if (is.list(m$w[[r]])){
+            dir.create(path = file.path(npath, sub("\\/", " ", r)))
+            for (s in names(m$w[[r]])){
+              if (all(class(m$w[[r]][[s]]) == c("NINA", "niche"))){
+                write_niche(m$w[[r]][[s]], path = file.path(npath,sub("\\/", " ", r)), file = s)
+              }
+            }
+          }
+        }
+      }
+    }
+    ####
+    #### species distribution
+    sd <- m$pred.dis
+    npath = file.path(path, "sd")
     dir.create(npath)
-    for (n in names(m$eval)) {write.table(m$eval[[n]],  file = paste0(npath, "/", n, ".txt"))}
-  }
-  ####
+    write.table(sd, file = paste0(npath, "/species_distributions.txt"))
+    sapply(names(m$maps) , function(s) writeRaster(m$maps[[s]], filename = paste0(npath, "/", gsub("\\.", "_", s)), format = "GTiff", overwrite = T))
+    ####
+    #### info
 
+    npath = file.path(path, "info")
+    dir.create(npath)
+    write.table(m$tab, file = paste0(npath, "/tab.txt"))
+    write.table(m$fail, file = paste0(npath, "/fail.txt"))
+    write.table(m$predictors, file = paste0(npath, "/predictors.txt"), row.names = F, col.names = F)
+    write.table(as.character(m$crs), file = paste0(npath, "/crs.txt"))
+    ####
+    #### data
+
+    npath = file.path(path, "data")
+    dir.create(npath)
+    if (!is.null(m$clus)){
+      write.table(m$clus, file = paste0(npath, "/regions.txt"))
+    }
+    write.table(m$obs, file = paste0(npath, "/occurrences.txt"))
+    write.table(m$sp.scores, file = paste0(npath, "/sp_scores.txt"))
+    write.table(m$env.scores, file = paste0(npath, "/env_scores.txt"))
+    saveRDS(m$pca, file = paste0(npath, "/pca.RDS"))
+
+    ####
+    #### eval
+    if (!is.null(m$eval)){
+      npath = file.path(path, "eval")
+      dir.create(npath)
+      for (n in names(m$eval)) {write.table(m$eval[[n]],  file = paste0(npath, "/", n, ".txt"))}
+    }
+    ####
+  }
+  else {
+    stop("Object is not of class NINA")
+  }
 }
